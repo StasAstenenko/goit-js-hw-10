d61714d4e6acb4b7215308f5f74c30c08a564869
StasAstenenko <107870729+StasAstenenko@users.noreply.github.com>
2024-06-10 12:55:05 +0000

    Deploying to gh-pages from @ StasAstenenko/goit-js-hw-10@5a77087a2ec6905aeb141b26380df7af686a9b72 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{i as r}from"./assets/vendor-77e16229.js";const m=document.querySelector(".form");m.addEventListener("submit",t=>{t.preventDefault();const s=t.target.delay.value,o=t.target.state.value;new Promise((e,i)=>{setTimeout(()=>{o==="fulfilled"?e(s):i(s)},s)}).then(e=>r.success({message:`✅ Fulfilled promise in ${e}ms`})).catch(e=>r.error({title:"Error",message:`❌ Rejected promise in ${e}ms`}))});
+//# sourceMappingURL=commonHelpers2.js.map
